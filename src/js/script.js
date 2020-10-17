5bea8d39be90104035abb88c9ced52f3b94646c1
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-17 21:07:48 +0100

    added onclick event for notify-me button

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
+let errorMsg = document.getElementById("error-message");
+let emailInput = document.getElementById("email-address");
+let submitBtn = document.getElementById("notify-me");
+
+
 function emailIsValid(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+
+submitBtn.onclick = () => {
+    return emailIsValid(emailInput.value) ? location.reload() : errorMsg.style.display = 'block';
 }
